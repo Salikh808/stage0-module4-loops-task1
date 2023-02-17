5c33f4b641b11ca42e22cb53126510837a3ad4d4
nurzhan <Salikh835@gmail.com>
2023-02-17 14:05:01 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,7 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        String s = "writing the same code doesn't have much impact, and it's also time-consuming";
+        System.out.println(s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s+" "+s);
     }
 }
